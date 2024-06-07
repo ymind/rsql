dcc7aee8ccac5db31b57f56dcf5ef73c3045b842
oc <oc@yi.team>
2024-06-07 20:38:16 +0800

    chore(deps): 更新依赖包版本

--- rsql-deps/build.gradle.kts
+++ rsql-deps/build.gradle.kts
@@ -15,9 +15,9 @@ dependencies {
     // https://mvnrepository.com/artifact/org.apache.logging.log4j/log4j-bom
     api(platform("org.apache.logging.log4j:log4j-bom:3.0.0-beta2"))
     // https://mvnrepository.com/artifact/org.jooq/jooq-parent
-    api(platform("org.jooq:jooq-parent:3.19.8"))
+    api(platform("org.jooq:jooq-parent:3.19.9"))
     // https://mvnrepository.com/artifact/com.mybatis-flex/mybatis-flex-dependencies
-    api(platform("com.mybatis-flex:mybatis-flex-dependencies:1.8.8"))
+    api(platform("com.mybatis-flex:mybatis-flex-dependencies:1.9.2"))
 
     constraints {
         // https://mvnrepository.com/artifact/ch.qos.logback/logback-classic
@@ -28,9 +28,9 @@ dependencies {
         // https://mvnrepository.com/artifact/io.github.oshai/kotlin-logging-jvm
         api("io.github.oshai:kotlin-logging-jvm:6.0.9")
         // https://mvnrepository.com/artifact/io.zeko/zeko-sql-builder
-        api("io.zeko:zeko-sql-builder:1.5.5")
+        api("io.zeko:zeko-sql-builder:1.5.6")
         // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-coroutines-jdk8
-        api("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.8.0")
+        api("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.8.1")
 
         // https://mvnrepository.com/artifact/com.h2database/h2
         api("com.h2database:h2:2.2.224")
